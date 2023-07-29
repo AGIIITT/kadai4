ec8998b6fad6d0658b2612518b38d30b549372d1
anon <z23033kx@aiit.ac.jp>
2023-07-29 13:19:54 +0000

    probably last commit

--- kadai4.sh
+++ kadai4.sh
@@ -4,6 +4,32 @@
 n1=$1
 n2=$2
 
+if [ "$#" -lt 2 ]; 
+then
+	echo "Need 2 inputs"
+	exit 1
+fi
+
+if [ "$#" -gt 2 ]; 
+then
+	echo "Warning: Inputs after the first 2 inputs will be ignored"
+fi
+
+if [[ $n1 =~ ^-?[0-9]+$ ]];
+then
+	:
+else
+	echo 'Input must be an integer'
+	exit 1
+fi
+
+if [[ $n2 =~ ^-?[0-9]+$ ]];
+then
+	:
+else
+	echo 'Input must be an integer'
+	exit 1
+fi
 
 while [ $n1 -gt -0 -a $n2 -gt 0 ]; do
 	if [ $n1 -gt $n2 ]; 
@@ -21,3 +47,5 @@ then
 else
 	echo $n2
 fi
+
+exit 0
---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Test 1: "
+./kadai4.sh 15 33
+
+echo "Test 2: "
+./kadai4.sh 1920 1080
+
+echo "Test 3: "
+./kadai4.sh -25 30
+
+echo "Test 4: "
+./kadai4.sh 2.5 35
+
+echo "Test 5: "
+./kadai4.sh notanumber 6
+
+echo "Test 6: "
+./kadai4.sh 5
+
+echo "Test 7: "
+./kadai4.sh 14 21 28
